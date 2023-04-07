573e69b048385945f9c77df6dc589c4f8b08c690
0xh7ml <darkhtml2275@gmail.com>
2023-04-07 11:33:23 +0600

    functions learning

--- functions.c
+++ functions.c
@@ -1,6 +1,14 @@
 //
 // Created by Saikat on 7/4/23.
 //
+
+// Functions type
+/*
+ * No aurguments , no return value
+ * No aurguments , return value
+ * Aurguments, no return value
+ * Aurguments, return value
+ */
 #include <stdio.h>
 int main(){
     printf("I;m learning functions");
