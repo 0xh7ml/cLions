fdc20d990b76de2272b587c124a7e0fc4b7c5a3e
0xh7ml <darkhtml2275@gmail.com>
2023-04-07 12:05:22 +0600

    functions learning

---
+++ inc_dec.c
@@ -0,0 +1,21 @@
+//
+// Created by Saikat on 7/4/23.
+//
+
+// post increment/decrement
+// pre increment/decrement
+#include <stdio.h>
+int main(){
+    int x;
+    int y = 3;
+
+    x = --y; // pre decrement of y
+    // pre decrement
+    // first decrese the value of y
+    // then assign the value to var or x
+
+
+    // first assigned the value x = 3
+    // then do decrement y = 2
+    printf("%d", y);
+}
